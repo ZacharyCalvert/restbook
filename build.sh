22180b99fba1b99e065fac240a6e1ec087613d4e
Zach Calvert <ZacharyCalvert+ghub@gmail.com>
2020-12-13 12:02:39 -0600

    Scripting out a PDF

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+rm restful.html
+rm *.fo
+rm restful.pdf
+
 pandoc --from docbook --to html --output restful.html restful.xml
-pandoc --from docbook --to pdf --output restful.pdf restful.xml
+# pandoc --from docbook --to latex --output restful.pdf restful.xml
+xsltproc -o intermediate-fo-file.fo /usr/share/xml/docbook/stylesheet/nwalsh/fo/docbook.xsl restful.xml
+fop -pdf restful.pdf -fo intermediate-fo-file.fo
